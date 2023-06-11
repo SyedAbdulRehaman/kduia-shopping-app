d4d42ff80c58f81327b1fb9a919833420d9c6622
Rehamangithub <136150739+SyedAbdulRehaman@users.noreply.github.com>
2023-06-11 19:58:25 +0530

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,38 @@
+// import React, { useContext } from 'react';
+// import { AppContext } from '../context/AppContext';
+
+// const ExpenseList = () => {
+// };
+
+// export default ExpenseList;
+
+
 import React, { useContext } from 'react';
+import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseList = () => {
+    const { expenses } = useContext(AppContext);
+
+    return (
+        <table className='table'>
+              <thead className="thead-light">
+            <tr>
+              <th scope="col">Department</th>
+              {/* <th scope="col">Quantity</th> */}
+              {/* <th scope="col">Unit Price</th> */}
+              <th scope="col">Allocated Budget</th>
+              <th scope="col">Increased By 10</th>
+              <th scope="col">Remove</th>
+            </tr>
+          </thead>
+            <tbody>
+            {expenses.map((expense) => (
+                <ExpenseItem id={expense.id} key={expense.id} name={expense.name} quantity={expense.quantity} unitprice={expense.unitprice} />
+            ))}
+            </tbody>
+        </table>
+    );
 };
 
 export default ExpenseList;
